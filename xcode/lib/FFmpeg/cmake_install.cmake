07531957e21a72dba20f265200c30ecd1d25570e
Shanika Wijerathna <shanikacj@gmail.com>
2020-05-19 22:10:15 +1200

    linked with ffmpeg

--- xcode/lib/FFmpeg/cmake_install.cmake
+++ xcode/lib/FFmpeg/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/shanikawijerathna/projects/camid/video-app
+# Install script for directory: /Users/shanikawijerathna/projects/camid/video-app/lib/FFmpeg
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,23 +32,3 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/shanikawijerathna/projects/camid/video-app/xcode/lib/glfw/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/shanikawijerathna/projects/camid/video-app/xcode/lib/FFmpeg/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/shanikawijerathna/projects/camid/video-app/xcode/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ xcode/video-app.build/Debug/ZERO_CHECK.build/Script-C8F066EC41794B5699A4E629.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/shanikawijerathna/projects/camid/video-app/xcode -f /Users/shanikawijerathna/projects/camid/video-app/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all

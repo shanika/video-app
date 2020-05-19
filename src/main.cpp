11087400302d937526e9a0a2d0c63aa4d477326a
Shanika Wijerathna <shanikacj@gmail.com>
2020-05-19 21:44:25 +1200

    USe OpenGL texture

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,58 @@ int main(int argc, const char** argv) {
 		}
 	}
 
+	for(int y = 25; y < 75; ++y) {
+		for (int x = 25; x < 75; ++x)
+		{
+			data[y * 100 * 3 + x * 3     ] = 0x00;
+			data[y * 100 * 3 + x * 3 + 1 ] = 0x00;
+			data[y * 100 * 3 + x * 3 + 2 ] = 0xff;
+		}
+	}
+
+	glfwMakeContextCurrent(window);
+
+	int width = 100;
+	int hight = 100;
+
+	GLuint tex_handle;
+	glGenTextures(1, &tex_handle);
+	glBindTexture(GL_TEXTURE_2D, tex_handle);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, hight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
 	glfwMakeContextCurrent(window);
 	while(!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glfwSwapBuffers(window);
 
+		int window_width, window_height;
+	
+		glfwGetFramebufferSize(window, &window_width, &window_height);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, window_width, 0, window_height, -1, 1);
+
+		glMatrixMode(GL_MODELVIEW);
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, tex_handle);
+		glBegin(GL_QUADS);
+			glTexCoord2d(0,0); glVertex2i(0,0);
+			glTexCoord2d(1,0); glVertex2i(width,0);
+			glTexCoord2d(1,1); glVertex2i(width,hight);
+			glTexCoord2d(0,1); glVertex2i(0,hight);
+		glEnd();
+		glDisable(GL_TEXTURE_2D);
+
+
+		glfwSwapBuffers(window);
 		glfwWaitEvents();   
 	}
 	
